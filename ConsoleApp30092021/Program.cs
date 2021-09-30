9851780538ce3d07c371378f333d9ec4a6f3f2ef
usuario <usuario@LENOVO-PC>
2021-09-30 17:35:19 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp30092021/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ConsoleApp30092021
+{
+    class motor
+    {
+        static void Main(string[] args)
+        {
+            //potencia CV
+            int M, S, V;
+            Console.WriteLine("ingresa el valor de M");
+            M = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingresa el valor de S");
+            S = int.Parse(Console.ReadLine());
+
+            int SUMA = M + S;
+            Console.WriteLine("los caballos de fuerza del V son" + SUMA);
+
+            Console.ReadKey();
+
+        }
+
+
+    }
+
+}
+
+
+
+
+
+namespace ConsoleApp30092021
+{
+    class vehiculos
+    {
+        static void Main(string[] args)
+        {
+            //color
+            int M, S, V;
+            Console.WriteLine("ingresa el color de M");
+            M = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingresa el color de S");
+            S = int.Parse(Console.ReadLine());
+            Console.WriteLine("los colores de M y S son");
+            Console.ReadKey();
+            //profe hasta aquí llego mi entendimiento, no esta completo pero lo intente
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
